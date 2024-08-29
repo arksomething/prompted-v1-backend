90822d846fcabab4864196fd67f3f95ba3f837d0
arksomething <ark296296@gmail.com>
2024-08-28 22:13:49 -0400

    added some token counting functions and creating prompt imports

--- index.js
+++ index.js
@@ -30,6 +30,21 @@ const client = new Client({
   port: 5432,
 });
 
+//prices in per million tokens
+const modelPricing = {
+  'gpt-4o-mini': {input: 0.150, output: 0.600},
+  'gpt-4o': {input: 5.00, output: 15.00},
+}
+
+const promptedPricing = {
+  'gpt-4o-mini': {input: 0.300, output: 1.200},
+  'gpt-4o': {input: 10.00, output: 30.00},
+}
+
+function roundToSecond(num) {
+  return(Math.round(num * 100) / 100)
+};
+
 client.connect()
   .then(() => console.log('Connected to PostgreSQL'))
   .catch(err => console.error('Connection error', err.stack));
@@ -42,7 +57,6 @@ const authMiddleware = (req, res, next) => {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
   try {
-    // Replace 'your_jwt_secret' with your actual secret key
     const decoded = jwt.verify(token, SECRET_KEY);
     req.id = decoded.id;
     next();
@@ -52,9 +66,9 @@ const authMiddleware = (req, res, next) => {
 };
 
 const app = express();
-app.use(['/login', '/register', '/user', '/chat', '/playground', '/testing', '/import/choices', '/import/data', '/create-checkout-session'], bodyParser.json());
+app.use(['/login', '/register', '/user', '/chat', '/playground', '/testing', '/import/choices', '/import/data', "/import/save", '/create-checkout-session'], bodyParser.json());
 app.use(cors());
-app.use(['/user', '/chat', '/playground', '/testing', '/import/choices', '/create-checkout-session'], authMiddleware)
+app.use(['/user', '/chat', '/playground', '/testing', '/import/choices', '/create-checkout-session', "/import/save"], authMiddleware)
 
 app.post('/register', async (req, res) => {
   const { username, password, email } = req.body;
@@ -128,115 +142,123 @@ app.get('/user', async (req, res) => {
   }
 });
 
-app.post("/chat", async (req, res) => {
-  const { chatState, systemPrompt, userPrompt, options } = req.body;
-  var newChatState = []
+async function parsePrompts(chatState, systemPrompts, userPrompt, options) {
 
-  if (chatState.length == 0) {
-    newChatState = [
-      {role: "system", content: systemPrompt}, 
-      {role: "user", content: userPrompt}
-    ]
-  } else {
-    newChatState = [
-      ...chatState, 
-      {role: "user", content: userPrompt}
-    ]
+  let systemPrompt = {role: 'system', content: 'You are an intelligent assistant'}
+  let finalUserPrompt = userPrompt
+  var layering = []
+  var inputCharacters = 0
+  var outputCharacters = 0
+  let newChatState = []
+
+  try {
+    for (const p of systemPrompts) {
+      if (p.role === 'system') {
+        systemPrompt = {role: 'system', content: p.content}
+      } else if (p.role === 'engineering') {
+        if (p.content.includes('${prompt}')) {
+          finalUserPrompt = p.content.replace("${prompt}", finalUserPrompt)
+        } else {
+          finalUserPrompt = finalUserPrompt + p.content
+        }
+        
+      } else if (p.role === 'layering') {
+        layering.push(p.content)
+      }
+    }
+
+    inputCharacters += finalUserPrompt.length
+
+    if (chatState.length == 0) {
+      newChatState = [
+        systemPrompt, 
+        {role: "user", content: finalUserPrompt}
+      ]
+    } else {
+      newChatState = [
+        ...chatState, 
+        {role: "user", content: finalUserPrompt}
+      ]
+    }
+  } catch (error) {
+    return(error)
   }
 
-  const moderation = await openai.moderations.create({input: userPrompt});
-
-  if (moderation.results[0].flagged == true) {
-    return res.send("This message was blocked by the ChatGPT Moderation.");
-  };
-
-  const completion = await openai.chat.completions.create({
-    messages: newChatState,
-    model: options.model,
-    frequency_penalty: options.frequency,
-    presence_penalty: options.presence,
-    response_format: options.response,
-    temperature: options.temperature,
-  });
+  try {
   
-  res.send(completion.choices[0].message.content);
+    const moderation = await openai.moderations.create({input: finalUserPrompt});
+
+    if (moderation.results[0].flagged == true) {
+      return "This message was blocked by the ChatGPT Moderation."
+    };
+
+    const completion = await openai.chat.completions.create({
+      messages: newChatState,
+      model: options.model,
+      frequency_penalty: options.frequency,
+      presence_penalty: options.presence,
+      response_format: options.response,
+      temperature: options.temperature,
+    });
+    
+    let completionContent = completion.choices[0].message.content;
+    outputCharacters += completionContent.length
+
+    for (const layer of layering) {
+      inputCharacters += layer.length
+      const moderation = await openai.moderations.create({input: layer});
+
+      if (moderation.results[0].flagged == true) {
+        return "This moderation was blocked by the ChatGPT Moderation."
+      };
+
+      const completion = await openai.chat.completions.create({
+        messages: [
+          {role: "system", content: layer},
+          {role: "user", content: completionContent}
+        ],
+        model: options.model,
+        frequency_penalty: options.frequency,
+        presence_penalty: options.presence,
+        response_format: options.response,
+        temperature: options.temperature,
+      });
+
+      completionContent = completion.choices[0].message.content;
+      outputCharacters += completionContent.length
+    }
+
+    // console.log(
+    //   roundToSecond(modelPricing[options.model].input * ((inputCharacters/4))), 
+    //   roundToSecond(modelPricing[options.model].output * ((outputCharacters/4)))
+    // );
+
+    return completionContent
+
+  } catch (error){
+    return error
+  }
+}
+
+app.post("/chat", async (req, res) => {
+  const { chatState, systemPrompts, userPrompt, options } = req.body;
+  res.send(await parsePrompts(chatState, systemPrompts, userPrompt, options));
+
 });
 
 app.post("/playground", async (req, res) => {
-  const { systemPrompt, engineeringPrompt, userPrompt, options } = req.body;
+  const { systemPrompts, engineeringPrompts, userPrompt, options } = req.body;
 
-  var engineeringMessage = '';
-  var vanillaMessage = '';
+  const vanillaMessage = await parsePrompts([], systemPrompts, userPrompt, options)
+  const engineeringMessage = await parsePrompts([], engineeringPrompts, userPrompt, options)
 
-  const moderation = await openai.moderations.create({input: userPrompt});
-
-  if (moderation.results[0].flagged == true) {
-    vanillaMessage = "This message was blocked by the ChatGPT Moderation."
-  } else {
-    const vanilla = await openai.chat.completions.create({
-      messages: [
-        {role: "system", content: systemPrompt}, 
-        {role: "user", content: userPrompt}
-      ],
-      model: "gpt-3.5-turbo-0125",
-      frequency_penalty: options.frequency,
-      presence_penalty: options.presence,
-      response_format: options.response,
-      temperature: options.temperature,
-    });
-
-    vanillaMessage = vanilla.choices[0].message.content
-  }
-
-  const engModeration = await openai.moderations.create({input: engineeringPrompt.replace("${prompt}", userPrompt)});
-
-  if (engModeration.results[0].flagged == true) {
-    engineeringMessage = "This message was blocked by the ChatGPT Moderation."
-  } else {
-    const engineering = await openai.chat.completions.create({
-      messages: [
-        {role: "system", content: systemPrompt}, 
-        {role: "user", content: engineeringPrompt.replace("${prompt}", userPrompt)}
-      ],
-      model: "gpt-3.5-turbo-0125",
-      frequency_penalty: options.frequency,
-      presence_penalty: options.presence,
-      response_format: options.response,
-      temperature: options.temperature,
-    });
-
-    engineeringMessage = engineering.choices[0].message.content
-  }
-
-  
   res.send({vanilla: vanillaMessage, engineering: engineeringMessage});
 });
 
 app.post("/testing", async (req, res) => {
-  const { systemPrompt, engineeringPrompt, userPrompt, index, options} = req.body;
 
-  const moderation = await openai.moderations.create({input: userPrompt});
-
-  if (moderation.results[0].flagged == true) {
-    return res.send({index: index, content: "This message was blocked by the ChatGPT Moderation."});
-  };
-
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {role: "system", content: systemPrompt}, 
-      {role: "user", content: engineeringPrompt.replace("${prompt}", userPrompt)}
-    ],
-    model: "gpt-3.5-turbo-0125",
-    frequency_penalty: options.frequency,
-    presence_penalty: options.presence,
-    response_format: options.response,
-    temperature: options.temperature,
-  });
-
-  const completionMessage = completion.choices[0].message.content
-
-    
-  res.send({index: index, content: completionMessage});
+  const { systemPrompts, userPrompt, index, options } = req.body;
+  res.send({index: index, content: await parsePrompts([], systemPrompts, userPrompt, options)});
 });
 
 app.post("/import/choices", async (req, res) => {
@@ -265,6 +287,31 @@ app.post("/import/data", async (req, res) => {
   }
 })
 
+app.post("/import/save", async (req, res) => {
+  const { userPrompts, saveName } = req.body;
+
+  try {
+    const result = await client.query("INSERT INTO user_prompts (name, prompts) VALUES ($1, $2) RETURNING id", 
+      [saveName, userPrompts]);
+    const id = result.rows[0].id;
+
+    await client.query(`UPDATE users SET user_prompts = user_prompts || $1::jsonb WHERE id = $2;`, 
+      [JSON.stringify([{ id: id, name: saveName }]), req.id]);
+
+    res.json({
+      id: id
+    });
+
+  } catch (error) {
+    if (error.code === '23505') { // Unique violation
+      res.status(409).send('Prompt name already exists');
+    } else {
+      console.log(error)
+      res.status(500).send('Error saving prompts');
+    }
+  }
+})
+
 app.post('/create-checkout-session', async (req, res) => {
 
   const session = await stripe.checkout.sessions.create({
@@ -278,7 +325,7 @@ app.post('/create-checkout-session', async (req, res) => {
           unit_amount: 500,
           
         },
-        quantity: 1,
+        quantity: 500,
       },
     ],
     mode: 'payment',
@@ -379,7 +426,6 @@ app.post('/webhook', bodyParser.raw({type: 'application/json'}), async (request,
 
 
 app.listen(PORT, () => {
-  console.log(endpointSecret)
   console.log(`Server is running on port: ${PORT}`);
 });
 
